f840ba35fd09f930bc3db6859cf655cd834588db
Lene-Ribeiro <lenecristinacarla@gmail.com>
2023-10-14 21:03:46 -0300

    concluido

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -6,6 +6,9 @@ type Usuario = {
     dataNacimento?: string
 }
 
+type UsuarioSemRg = Omit<Usuario, 'rg'>
+type UsuarioObg = Required<UsuarioSemRg>
+
 const cadastrarUsuário = (info: Usuario): Usuario => {
     return info;
 }
